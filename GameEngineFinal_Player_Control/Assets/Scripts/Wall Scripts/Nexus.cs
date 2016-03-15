53c5293d2c30817d3567980bc137fc38ec8bd674
marcofilippobruno <351055@AIV.EDMC.STU>
2016-03-15 15:47:10 -0700

    Nexus added?

---
+++ GameEngineFinal_Player_Control/Assets/Scripts/Wall Scripts/Nexus.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nexus : Walls {
+
+	// Use this for initialization
+	void Start () {
+
+        repairAmount = 15;
+
+        upgradeOneHealth = 550;
+        upgradeTwoHealth = 700;
+
+        // set start hp vals //
+        maxWallHealth = 500;
+        currentWallHealth = 500;
+
+        // array for repair reqs //
+        repairReq[0] = 10;
+        repairReq[1] = 15;
+        repairReq[2] = 15;
+
+        // array for upgrade reqs //
+        upgradeReq[0] = 30;
+        upgradeReq[1] = 40;
+        upgradeReq[2] = 40;
+
+	}
+	
+
+
+   
+}
